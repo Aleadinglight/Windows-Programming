f7c865191ad7e7aff7e3bc4ad57d9fda4cc19d75
Aleadinglight <huutamnguyen2302@gmail.com>
2018-09-26 00:57:07 +0300

    added full solution to hw1

--- hw1/hw1/Matrix.cs
+++ hw1/hw1/Matrix.cs
@@ -8,11 +8,64 @@ namespace hw1
 {
     class Matrix
     {
-        private int width, height;
+        private int width,height;
+        private int[,] array;
+        public static Random randNum = new Random();
+        public int getWidth
+        {
+            get { return width; }
+        }
+
+        public int getHeight
+        {
+            get { return height; }
+        }
+
         public Matrix(int width, int height)
         {
             this.width = width;
             this.height = height;
+            this.array = new int[this.width, this.height];
+            for (int i = 0; i < this.width; i++)
+                for (int j = 0; j < this.height; j++)
+                    this.array[i, j] = randNum.Next(1, 10);
         }
+
+        public int[,] value()
+        {
+            return this.array;
+        }
+
+        public Matrix add( Matrix b)
+        {
+            // Create another matrix with the same size as a & b
+            Matrix c = new Matrix(this.width, this.height);
+            for (int i = 0; i < c.width; i++)
+                for (int j = 0; j < c.height; j++)
+                    c.value()[i, j] = this.value()[i, j] + b.value()[i, j];
+            return c;
+        }
+
+        public Matrix minus(Matrix b)
+        {
+            // Create another matrix with the same size as a & b
+            Matrix c = new Matrix(this.width, this.height);
+            for (int i = 0; i < c.width; i++)
+                for (int j = 0; j < c.height; j++)
+                    c.value()[i, j] = this.value()[i, j] - b.value()[i, j];
+                   
+            return c;
+        }
+
+        public Matrix mul(Matrix b)
+        {
+            // Create another matrix with the same size as a & b
+            Matrix c = new Matrix(this.width, this.height);
+            for (int i = 0; i < c.width; i++)
+                for (int j = 0; j < c.height; j++)
+                    c.value()[i, j] = this.value()[i, j] * b.value()[i, j];
+            return c;
+        }
+
     }
 }
--- hw1/hw1/Program.cs
+++ hw1/hw1/Program.cs
@@ -8,8 +8,44 @@ namespace hw1
 {
     class Program
     {
+        // This procedure is used to print out the matrix
+        static void writeMatrix(Matrix n)
+        {
+            for (int i = 0; i < n.getWidth; i++)
+            {
+                for (int j = 0; j < n.getHeight; j++)
+                    Console.Write(n.value()[i, j]+" ");
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
         static void Main(string[] args)
         {
+            // Define the sizes for the random matrices
+            Random randNum = new Random();
+            int width = randNum.Next(1, 5);
+            int height = randNum.Next(1, 5);
+
+            // Create 2 random matrices
+            Matrix a = new Matrix(width, height);
+            Matrix b = new Matrix(width, height);
+            // Output the matrices to the screen
+            Console.Write("A\n");
+            writeMatrix(a);
+            Console.Write("B\n");
+            writeMatrix(b);
+
+            // Perform basic operation 
+            Matrix sum = a.add(b);
+            Matrix diff = a.minus(b);
+            Matrix product= a.mul(b);
+            // Output the results
+            Console.Write("A+B\n");
+            writeMatrix(sum);
+            Console.Write("A-B\n");
+            writeMatrix(diff);
+            Console.Write("A*B\n");
+            writeMatrix(product);
         }
     }
 }

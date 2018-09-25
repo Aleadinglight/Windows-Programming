f9fe139c51481b86bfd1e6bee167bd1b42f531b5
Aleadinglight <huutamnguyen2302@gmail.com>
2018-09-25 23:49:24 +0300

    added constructor for matrix

---
+++ hw1/hw1/Matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    class Matrix
+    {
+        private int width, height;
+        public Matrix(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+}

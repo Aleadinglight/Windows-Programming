f8eca46fb09a31451dc8b1f44370fb261766d34f
Aleadinglight <huutamnguyen2302@gmail.com>
2018-10-01 11:00:00 +0100

    added toString method to Customer

--- hw2/hw2/Customer.cs
+++ hw2/hw2/Customer.cs
@@ -8,10 +8,20 @@ namespace hw2
 {
     class Customer
     {
-        public Customer(string name, string id, string flightId)
-        {
+        private string name;
+        private int id, flightId;
 
+        public Customer(string name, int id, int flightId)
+        {
+            this.name = name;
+            this.id = id;
+            this.flightId = flightId;
         }
-            //git commit --date="Mon Oct 1 10:00 2018 +0100" -m "added customer class"
+        
+        public string toString()
+        {
+            return "Name: " + name + "Id: " + id + "Flight Id: " + flightId;
+        }
+            //git commit --date="Mon Oct 1 11:00 2018 +0100" -m "added customer class"
     }
 }

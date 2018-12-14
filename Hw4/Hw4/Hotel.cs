cac78f4c2b8f7fae5ecce6265592e4e5f975ce9d
Aleadinglight <huutamnguyen2302@gmail.com>
2018-12-14 05:03:48 +0200

    added Hotel class and Room Class

---
+++ Hw4/Hw4/Hotel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw4
+{
+    class Hotel
+    {
+        private string name, constructDate, address;
+        private int stars;
+        private List<Room> roomList;
+        private List<Customer> customerList;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string ConstructDate
+        {
+            get
+            {
+                return this.constructDate;
+            }
+            set
+            {
+                this.constructDate = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return this.address;
+            }
+            set
+            {
+                this.address = value;
+            }
+        }
+        public int Stars
+        {
+            get
+            {
+                return this.stars;
+            }
+            set
+            {
+                this.stars = value;
+            }
+        }
+
+        public Hotel(string name, string constructDate, string address, int stars)
+        {
+            this.name = name;
+            this.constructDate = constructDate;
+            this.address = address;
+            this.stars = stars;
+        }
+
+        public Hotel(string name, string constructDate, string address, int stars, List<Room> roomList, List<Customer> customerList)
+        {
+            this.name = name;
+            this.constructDate = constructDate;
+            this.address = address;
+            this.stars = stars;
+            this.roomList = roomList;
+            this.customerList = customerList;
+        }
+
+        public void saveHotelData()
+        {
+
+        }
+    }
+}
---
+++ Hw4/Hw4/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw4
+{
+    class Room
+    {
+        private int roomNumber, area;
+        private string type, description;
+        private double price;
+
+        public Room(int roomNumber, int area, string type, double price, string description)
+        {
+            this.roomNumber = roomNumber;
+            this.area = area;
+            this.type = type;
+            this.price = price;
+            this.description = description;
+        }
+
+
+    }
+}
